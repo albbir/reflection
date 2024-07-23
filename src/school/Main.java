514a03b78631a346b057677355abc13b10dbab6d
Albert Baymler <albert.baymler@itdep.net>
2024-07-23 14:48:48 +0500

    Java code Convention

--- src/school/Main.java
+++ src/school/Main.java
@@ -7,21 +7,21 @@ public class Main {
     public static void main(String[] args) throws IllegalAccessException {
         Teacher teacher = new Teacher();
         /* Class<Teacher> TeacherClass = teacher.class; */
-        Class<? extends Teacher> TeacherClass = teacher.getClass();
+        Class<? extends Teacher> Teacher = teacher.getClass();
 
-        Field[] declaredFields = TeacherClass.getDeclaredFields();
+        Field[] declaredFields = Teacher.getDeclaredFields();
         System.out.println("\nFields:");
         for (Field field : declaredFields) {
             System.out.println(field.getName()+"/"+field);
         }
 
-        Constructor[] constructorUser = TeacherClass.getDeclaredConstructors();
+        Constructor[] constructorUser = Teacher.getDeclaredConstructors();
         System.out.println("Constructors:");
         for (Constructor constructor : constructorUser) {
             System.out.println(constructor.getName()+"/"+constructor);
         }
 
-        Method[] declaredMethods = TeacherClass.getDeclaredMethods();
+        Method[] declaredMethods = Teacher.getDeclaredMethods();
         System.out.println("\nMethods:");
         for (Method method : declaredMethods) {
             System.out.println(method.getName()+"/"+method);
